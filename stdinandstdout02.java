9999f032febe31975768818e2cad0773f55b73ec
Tolichukka Akanksha <125852692+TolichukkaAkanksha@users.noreply.github.com>
2023-04-12 14:58:26 +0530

    Add files via upload

---
+++ stdinandstdout02.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class stdinandstdout02 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+         int a2 = scan.nextInt();
+          int a3= scan.nextInt();
+       System.out.println(a);
+        System.out.println(a2);
+        System.out.println(a3); 
+        
+    }
+}
